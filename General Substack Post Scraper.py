f6526600f463603ce5225bb6ec421519a20d665a
randydongshi <116231776+randydongshi@users.noreply.github.com>
2023-01-26 16:56:36 -0500

    Update General Substack Post Scraper.py

--- General Substack Post Scraper.py
+++ General Substack Post Scraper.py
@@ -41,64 +41,62 @@ def substack_post_scraper(url):
     #Setup csv files (doing 2 files: file1 is for the post itself; file 2 is for the comments
     file1 = open(input('Name the file for post content data'), 'w')
     writer1 = csv.writer(file1)
-    writer1.writerow(['Post Link', 'Word Count'])
+    writer1.writerow(['Post Link', 'Post Content', 'Word Count'])
 
     file2 = open(input('Name the file for post comment data'), 'w')
     writer2 = csv.writer(file2)
-    writer2.writerow(['Post Link', 'Commenter', 'Commenter Writes'])
+    writer2.writerow(['Post Link', 'Commenter', 'Comment', 'Commenter Writes'])
 
-    #Scrape post
-    linksraw = browser.find_elements_by_css_selector('a.post-preview-description')
-    links = []
-    for link in linksraw:
-        links.append(link.get_attribute('href'))
+#Scrape post
+linksraw = browser.find_elements_by_css_selector('a.post-preview-description')
+links = []
+for link in linksraw:
+    links.append(link.get_attribute('href'))
 
-    for link in links:
-        browser.get(link)
+for link in links:
+    browser.get(link)
+    try:
+        post_content_raw = browser.find_element_by_css_selector('div.available-content > div').text
+        wordcount = len(post_content.replace(',', '').split())
+    except:
         try:
-            post_content = browser.find_element_by_css_selector('div.available-content > div').text
+            post_content_raw = browser.find_element_by_css_selector('td > div').text
             wordcount = len(post_content.replace(',', '').split())
         except:
-            try:
-                post_content = browser.find_element_by_css_selector('td > div').text
-                wordcount = len(post_content.replace(',', '').split())
-            except:
-                post_content = browser.find_element_by_class_name('body markup').text
-                wordcount = len(post_content.replace(',', '').split())
+            post_content_raw = browser.find_element_by_class_name('body markup').text
+            wordcount = len(post_content.replace(',', '').split())
+    try:
+        writer1.writerow([link, post_content, wordcount])
+    except:
+        writer1.writerow([link.encode('utf-8'), post_content.encode('utf-8'), wordcount])
+    try:
+        comment_link_raw = browser.find_element_by_css_selector('div > div.single-post-section.comments-section > div.container > a')
+        comment_link = comment_link_raw.get_attribute('href')
+    except:
+        comment_link = None
+    #Scrape post comments
+    try:
+        browser.get(comment_link)
+    except:
+        pass
+    time.sleep(2)
+    comments = browser.find_elements_by_css_selector('div.comment-list > div > div')
+    for comment in comments:
         try:
-            writer1.writerow([link, wordcount])
+            commenter = comment.find_element_by_css_selector('td.comment-rest > div.comment-meta > span').text
         except:
-            writer1.writerow([link.encode('utf-8'), wordcount])
+            commenter = "Error"
         try:
-            comment_link_raw = browser.find_element_by_css_selector('div > div.single-post-section.comments-section > div.container > a')
-            comment_link = comment_link_raw.get_attribute('href')
+            actual_comment = comment.find_element_by_class_name('comment-body').text
         except:
-            comment_link = None
-        #Scrape post comments
+            actual_comment = "Error"
         try:
-            browser.get(comment_link)
+            commenter_writes = comment.find_element_by_css_selector('a.commenter-publication > span').text
         except:
-            pass
-        time.sleep(2)
-        comments = browser.find_elements_by_css_selector('div.comment-list > div > div')
-        for comment in comments:
-            try:
-                commenter = comment.find_element_by_css_selector('td.comment-rest > div.comment-meta > span').text
-            except:
-                commenter = "Error"
-            if commenter == 'Matt Taibbi':
-                commenter_writes = "TK News by Matt Taibbi"
-            else:
-                try:
-                    commenter_writes = comment.find_element_by_css_selector('a.commenter-publication > span').text
-                except:
-                    commenter_writes = ''
-            try:
-                writer2.writerow([link, commenter, commenter_writes])
-            except:
-                writer2.writerow([link.encode('utf-8'), commenter.encode('utf-8'), commenter_writes.encode('utf-8')])
-    file.close()
+            commenter_writes = ''
+        try:
+            writer2.writerow([link, commenter, actual_comment, commenter_writes])
+        except:
+            writer2.writerow([link.encode('utf-8'), commenter.encode('utf-8'), actual_comment.encode('utf-8'), commenter_writes.encode('utf-8')])
 
 data = substack_scraper(input('Put in the archive link of the writer you want to scrape?'))
-
-#MattTaibbiSubstack = 'https://taibbi.substack.com/archive'
